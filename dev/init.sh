fd5b30a07f2218617d9435ef2a10fca3a186cb2d
adrian-kriegel <adrian@unolog.in>
2022-09-05 09:25:21 +0200

    Using python 3.10

--- dev/init.sh
+++ dev/init.sh
@@ -4,7 +4,7 @@ sh dev/install_git_hooks.sh
 
 if [ ! -d python_env ]; then
 
-  virtualenv -p python3.11 python_env
+  virtualenv python_env
   source python_env/bin/activate
   pip install -r requirements.txt
 
